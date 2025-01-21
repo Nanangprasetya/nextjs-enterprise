5500a2f1fc8f26d66ad98e8adeecf35c158fa184
Nanang Prasetya Bekti <nanangprasetya251015@gmail.com>
2025-01-21 17:30:23 +0700

    initial commit

---
+++ madge.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  excludePatterns: [
+    '.next',
+    'tailwind.config.js',
+    'reset.d.ts',
+    'prettier.config.js',
+    'postcss.config.js',
+    'playwright.config.ts',
+    'next.config.js',
+    'next-env.d.ts',
+    'instrumentation.ts',
+    'e2e/',
+    'README.md',
+    '.storybook/',
+    'components/',
+    '.eslintrc.js',
+    'styles/tailwind.css'
+  ]
+};  
